0815b3f98581c03fe7277aa80a9b0fc095571e48
Dhava Rachmana Aziz <dhava.rachmana.aziz@gmail.com>
2021-10-05 13:06:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moviedb/viewmodel/MovieViewModel.java
@@ -0,0 +1,33 @@
+package com.example.moviedb.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.moviedb.model.Movies;
+import com.example.moviedb.repositories.MovieRepository;
+
+public class MovieViewModel extends AndroidViewModel {
+
+    private MovieRepository repository;
+
+    public MovieViewModel(@NonNull Application application){
+        super(application);
+        repository = MovieRepository.getInstance();
+    }
+
+    //==Begin of viewmodel get movie by id
+    private MutableLiveData<Movies> resultGetMovieById = new MutableLiveData<>();
+
+    public void getMovieById(String movieId){
+        resultGetMovieById = repository.getMovieData(movieId);
+    }
+    public LiveData<Movies> getResultGetMovieById(){
+        return resultGetMovieById;
+    }
+
+    //==End of viewmodel get movie by id
+}
