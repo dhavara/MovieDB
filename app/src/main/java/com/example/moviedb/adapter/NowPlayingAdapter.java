13df8b4e515c39632b2fc24d7cf59dfc79894be4
dhavara <dhava.rachmana.aziz@gmail.com>
2021-10-19 12:27:28 +0700

    update fragment

--- app/src/main/java/com/example/moviedb/adapter/NowPlayingAdapter.java
+++ app/src/main/java/com/example/moviedb/adapter/NowPlayingAdapter.java
@@ -2,6 +2,7 @@ package com.example.moviedb.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -51,10 +53,15 @@ public class NowPlayingAdapter extends RecyclerView.Adapter<NowPlayingAdapter.Ca
                 .into(holder.img_poster);
         holder.cv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, MovieDetailsActivity.class);
-                intent.putExtra("movie_id", ""+results.getId());
-                context.startActivity(intent);
+            public void onClick(View view) {
+//                Intent intent = new Intent(context, MovieDetailsActivity.class);
+//                intent.putExtra("movie_id", ""+results.getId());
+//                context.startActivity(intent);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("movieId", ""+results.getId());
+                Navigation.findNavController(view).navigate(R.id
+                        .action_nowPlayingFragment_to_movieDetailsFragment, bundle);
             }
         });
     }
